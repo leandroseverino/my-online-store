4a538cfe74e6b626ada6cfe9b4dae3262e397e37
leandroseverino <lseverino@gmail.com>
2017-10-29 08:36:52 -0200

    All code

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,44 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div id="app" class="container my-5">
+    <div class="row mb-3">
+      <div class="col-md-9">
+        <h1>My online store</h1>
+      </div>
+      <div class="col-md-3">
+        <ShoppingCart />
+      </div>
+    </div>
+     <div class="row">
+      <Item
+        v-for="item in forSale"
+        :key="item.invId"
+        :invId="item.invId"
+        :name="item.name"
+        :image="item.image"
+        :price="item.price" />
+    </div>
+    {{ inCart }}
   </div>
 </template>
-
 <script>
+import Item from "./Item.vue";
+import ShoppingCart from "./ShoppingCart.vue";
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  name: "app",
+  computed: {
+    forSale() {
+      return this.$store.getters.forSale;
+    },
+    inCart() {
+      return this.$store.getters.inCart;
     }
+  },
+  components: {
+    Item,
+    ShoppingCart
   }
-}
+};
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+/* Nothing for now */
 </style>
